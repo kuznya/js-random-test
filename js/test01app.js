fc6103b78e095a28ad5db951c0407511dd9f3e3c
kuznya <kuznya1@gmail.com>
2024-07-26 20:56:23 +0300

    refact testXXapp.js

--- js/test01app.js
+++ js/test01app.js
@@ -1,42 +1,42 @@
 // test01app.js
 // test 01 - distribution d10 test x10
 
-  const renderHeader = () => {
-    return (
-      '<div class="header">' +
-      '<button id="btn" onclick="go();">redo</button>' +
-      '</div>'
-    );
-  };
+const renderHeader = () => {
+	return (
+		'<div class="header">' +
+		'<button id="btn" onclick="go();">redo</button>' +
+		'</div>'
+	);
+};
 
-  const app = new ConsoleApp(renderHeader);
-  // go!
-  const limit = 10;
-  const n = 1000;
-  const steps = 10;
+const app = new ConsoleApp(renderHeader);
+// go!
+const limit = 10;
+const n = 1000;
+const steps = 10;
 
-  const makeStep = (dist, dist_all) => {
-    for (let i = 0; i < n; i++) {
-      const v = Math.floor(Math.random() * limit);
-      dist.reg(v);
-      dist_all.reg(v);
-    }
-    return dist.stats();
-  };
+const makeStep = (dist, dist_all) => {
+	for (let i = 0; i < n; i++) {
+		const v = Math.floor(Math.random() * limit);
+		dist.reg(v);
+		dist_all.reg(v);
+	}
+	return dist.stats();
+};
 
-	const go = () => {
-		const dist = new cDistribution(limit);
-		const dist_all = new cDistribution(limit);
-		const aa = [];
-		for (let step = 1; step < steps + 1; step++) {
-			dist.reset();
-			const stats = makeStep(dist, dist_all);
-			aa.push(`step ${step}: ${app.formatDict(stats)}`)
-		}
-		const stats = dist_all.stats();
-		aa.push(`dict all: ${app.formatDict(stats)}`);
+const go = () => {
+	const dist = new cDistribution(limit);
+	const dist_all = new cDistribution(limit);
+	const aa = [];
+	for (let step = 1; step < steps + 1; step++) {
+		dist.reset();
+		const stats = makeStep(dist, dist_all);
+		aa.push(`step ${step}: ${app.formatDict(stats)}`)
+	}
+	const stats = dist_all.stats();
+	aa.push(`dict all: ${app.formatDict(stats)}`);
 
-		app.log.putlines(aa);
-	};
+	app.log.putlines(aa);
+};
 
-	go();
+go();
