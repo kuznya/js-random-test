7a9b319513e39f11f2e4db46599ffb86d41914da
kuznya <kuznya1@gmail.com>
2024-08-10 20:34:50 +0300

    refact consoleApp

--- js/consoleApp.js
+++ js/consoleApp.js
@@ -1,63 +1,62 @@
 class cLog {
-  constructor (area) {
-    this.area = area;
-  }
+	constructor(area) {
+		this.area = area;
+	}
 
-  topprint(s) {
-    const v = this.area.value;
-    this.area.value = v ? `${s}\n${v}` : s;
-  }
+	topprint(s) {
+		const v = this.area.value;
+		this.area.value = v ? `${s}\n${v}` : s;
+	}
 
-  print(s) {
-    const v = this.area.value;
-    this.area.value = v ? `${v}\n${s}` : s;
-  }
+	print(s) {
+		const v = this.area.value;
+		this.area.value = v ? `${v}\n${s}` : s;
+	}
 
-  putlines(lines) {
-    this.area.value = lines.join('\n')
-  }
+	putlines(lines) {
+		this.area.value = lines.join('\n')
+	}
 
-  clear() {
-    this.area.value = '';
-  }
+	clear() {
+		this.area.value = '';
+	}
 }
-  const write = (s) => { document.write(s); };
-  const elem = (id) => document.getElementById(id);
+
+const write = (s) => document.write(s);
+const elem = (id) => document.getElementById(id);
 
 class ConsoleApp {
-  constructor(renderHeader) {
-    this.logAreaId = 'logArea';
-    this.renderHeader = renderHeader ? renderHeader : () => {};
-    this.drawApp();
-    this.log = new cLog(elem(this.logAreaId));
-  }
+	constructor(renderHeader) {
+		this.logAreaId = 'logArea';
+		this.renderHeader = renderHeader ? renderHeader : () => {};
+		this.drawApp();
+		this.log = new cLog(elem(this.logAreaId));
+	}
 
-  drawApp () {
-    write('<div id="app">\n');
-    write(this.renderHeader());
-    write(`<textarea id="${this.logAreaId}" readonly></textarea>\n`);
-    write('</div>\n');
-  }
+	drawApp() {
+		write('<div id="app">\n');
+		write(this.renderHeader());
+		write(`<textarea id="${this.logAreaId}" readonly></textarea>\n`);
+		write('</div>\n');
+	}
 
-  print(s) {
-    this.log.print(s)
-  }
+	print(s) {
+		this.log.print(s)
+	}
 
 
-  formatNumber (value, places) {
-    const len = value.toString().length;
-    const spaces = Math.max(0, places - len);
-    return `${' '.repeat(spaces)}${value}`;
-  };
+	formatNumber(value, places) {
+		const len = value.toString().length;
+		const spaces = Math.max(0, places - len);
+		return `${' '.repeat(spaces)}${value}`;
+	};
 
-  formatDict (dict) {
+  formatDict(dict) {
     const aa = [];
     for (const k in dict) {
-      if (!dict.hasOwnProperty(k)) {
-        continue;
-      }
+      if (!dict.hasOwnProperty(k)) { continue; }
       const value = dict[k];
-      let repr = typeof value === 'string' ? `"${value}"` : value;
+      const repr = typeof value === 'string' ? `"${value}"` : value;
       aa.push(`${k}: ${repr}`);
     }
     return `{${aa.join(', ')}}`;
